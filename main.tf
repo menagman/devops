c48e02a1b505a2223afde80d4a4c5fa53c099ce9
Menaka <menamailbox@gmail.com>
2025-07-08 13:03:41 -0700

    Initial commit of Terraform main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+   region = "us-west2"
+}
+
+resource "aws_instance" "webserver" {
+   ami           = "ami-05ee755be0cd7555c"
+   instance_type = "t2.micro"
+   tags = {
+     Name = "jenkins-demo"
+   }
+}
